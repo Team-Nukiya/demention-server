5bab8b3d61d934521e16b9a41424876309671f22
alsdl0629 <ehfehf0629@gmail.com>
2024-01-07 23:28:45 +0900

    Init Project

---
+++ src/main/kotlin/team/nukiya/demention/DementionApplication.kt
@@ -0,0 +1,11 @@
+package team.nukiya.demention
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class DementionApplication
+
+fun main(args: Array<String>) {
+    runApplication<DementionApplication>(*args)
+}
---
+++ src/test/kotlin/team/nukiya/demention/DementionApplicationTests.kt
@@ -0,0 +1,13 @@
+package team.nukiya.demention
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class DementionApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}

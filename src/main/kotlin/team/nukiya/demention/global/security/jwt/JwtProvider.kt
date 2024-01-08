0f14c077fd4440009ee880354c47c523c5776d2d
alsdl0629 <ehfehf0629@gmail.com>
2024-01-08 21:32:46 +0900

    Chore: (main) 주석

--- src/main/kotlin/team/nukiya/demention/global/security/jwt/JwtProvider.kt
+++ src/main/kotlin/team/nukiya/demention/global/security/jwt/JwtProvider.kt
@@ -10,51 +10,51 @@ import team.nukiya.demention.global.security.jwt.JwtConstant.REFRESH
 import java.time.LocalDateTime
 import java.util.Date
 
-@Component
-class JwtProvider(
-    private val jwtProperties: JwtProperties,
-    private val refreshTokenRepository: RefreshTokenRepository
-) {
-    fun generateAllToken(id: String, authority: Authority): TokenResponse {
-        val accessToken = generateAccessToken(id, authority)
-        val refreshToken = generateRefreshToken(id, authority)
-
-        refreshTokenRepository.save(
-            RefreshToken(
-                accountId = id,
-                token = refreshToken
-            )
-        )
-
-        return TokenResponse(
-            accessToken,
-            refreshToken,
-            LocalDateTime.now().plusSeconds(jwtProperties.accessExp),
-            LocalDateTime.now().plusSeconds(jwtProperties.refreshExp)
-        )
-    }
-
-    fun generateAccessToken(id: String, authority: Authority) = generateToken(
-        id,
-        authority,
-        ACCESS,
-        jwtProperties.accessExp
-    )
-
-    fun generateRefreshToken(id: String, authority: Authority) = generateToken(
-        id,
-        authority,
-        REFRESH,
-        jwtProperties.refreshExp
-    )
-
-    private fun generateToken(id: String, authority: Authority, type: String, exp: Long) =
-        Jwts.builder()
-            .setHeaderParam(Header.JWT_TYPE, type)
-            .setSubject(id)
-            .claim(AUTHORITY, authority.name)
-            .signWith(jwtProperties.secretKey, SignatureAlgorithm.HS256)
-            .setIssuedAt(Date())
-            .setExpiration(Date(System.currentTimeMillis() + exp * 1000))
-            .compact()
-}
+//@Component
+//class JwtProvider(
+//    private val jwtProperties: JwtProperties,
+//    private val refreshTokenRepository: RefreshTokenRepository
+//) {
+//    fun generateAllToken(id: String, authority: Authority): TokenResponse {
+//        val accessToken = generateAccessToken(id, authority)
+//        val refreshToken = generateRefreshToken(id, authority)
+//
+//        refreshTokenRepository.save(
+//            RefreshToken(
+//                accountId = id,
+//                token = refreshToken
+//            )
+//        )
+//
+//        return TokenResponse(
+//            accessToken,
+//            refreshToken,
+//            LocalDateTime.now().plusSeconds(jwtProperties.accessExp),
+//            LocalDateTime.now().plusSeconds(jwtProperties.refreshExp)
+//        )
+//    }
+//
+//    fun generateAccessToken(id: String, authority: Authority) = generateToken(
+//        id,
+//        authority,
+//        ACCESS,
+//        jwtProperties.accessExp
+//    )
+//
+//    fun generateRefreshToken(id: String, authority: Authority) = generateToken(
+//        id,
+//        authority,
+//        REFRESH,
+//        jwtProperties.refreshExp
+//    )
+//
+//    private fun generateToken(id: String, authority: Authority, type: String, exp: Long) =
+//        Jwts.builder()
+//            .setHeaderParam(Header.JWT_TYPE, type)
+//            .setSubject(id)
+//            .claim(AUTHORITY, authority.name)
+//            .signWith(jwtProperties.secretKey, SignatureAlgorithm.HS256)
+//            .setIssuedAt(Date())
+//            .setExpiration(Date(System.currentTimeMillis() + exp * 1000))
+//            .compact()
+//}

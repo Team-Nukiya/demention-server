85732351f5d85d63fe501c3129066e116ee01a64
Kangmin <ehfehf0629@gmail.com>
2024-01-08 19:01:49 +0900

    Feat: (main) HealthCheck

---
+++ src/main/kotlin/team/nukiya/demention/domain/HealthCheckController.kt
@@ -0,0 +1,12 @@
+package team.nukiya.demention.domain
+
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RequestMapping("/health-check")
+@RestController
+class HealthCheckController {
+    @GetMapping
+    fun healthCheck() = "OK"
+}

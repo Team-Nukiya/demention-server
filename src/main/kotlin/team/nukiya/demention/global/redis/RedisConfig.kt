f4ac093d7450b8e9a1befe7099be152657f141e4
Kangmin <ehfehf0629@gmail.com>
2024-01-08 14:14:49 +0900

    Feat: (#4) Redis 관련 모듈 추가

---
+++ src/main/kotlin/team/nukiya/demention/global/redis/RedisConfig.kt
@@ -0,0 +1,28 @@
+package team.nukiya.demention.global.redis
+
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.data.redis.connection.RedisConnectionFactory
+import org.springframework.data.redis.connection.RedisPassword
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory
+import org.springframework.data.redis.core.RedisKeyValueAdapter
+import org.springframework.data.redis.repository.configuration.EnableRedisRepositories
+
+@EnableRedisRepositories(enableKeyspaceEvents = RedisKeyValueAdapter.EnableKeyspaceEvents.ON_STARTUP)
+@Configuration
+class RedisConfig(
+    private val redisProperties: RedisProperties
+) {
+
+    @Bean
+    fun redisConnectionFactory(): RedisConnectionFactory {
+        val redisConfiguration = RedisStandaloneConfiguration().apply {
+            hostName = redisProperties.host
+            port = redisProperties.port
+            password = RedisPassword.of(redisProperties.password)
+        }
+
+        return LettuceConnectionFactory(redisConfiguration)
+    }
+}
---
+++ src/main/kotlin/team/nukiya/demention/global/redis/RedisProperties.kt
@@ -0,0 +1,10 @@
+package team.nukiya.demention.global.redis
+
+import org.springframework.boot.context.properties.ConfigurationProperties
+
+@ConfigurationProperties(prefix = "spring.data.redis")
+data class RedisProperties(
+    val host: String,
+    val port: Int,
+    val password: String
+)

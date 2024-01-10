f0e8e7a321dfe2c59d8388bae095539ef61eeead
Kangmin <ehfehf0629@gmail.com>
2024-01-10 13:51:13 +0900

    Chore: (#14) ttl 수정

--- src/main/kotlin/team/nukiya/demention/domain/auth/domain/AuthCodeEntity.kt
+++ src/main/kotlin/team/nukiya/demention/domain/auth/domain/AuthCodeEntity.kt
@@ -4,7 +4,7 @@ import org.springframework.data.annotation.Id
 import org.springframework.data.redis.core.RedisHash
 import org.springframework.data.redis.core.index.Indexed
 
-@RedisHash(timeToLive = 300)
+@RedisHash(timeToLive = 180)
 class AuthCodeEntity(
     @Id
     val code: String,

12922bdd2a1ead2613f41574a13558f2345fbcb1
Kangmin <ehfehf0629@gmail.com>
2024-01-10 13:00:24 +0900

    Chore: (#1) UserEntityRepository로 변경

--- src/main/kotlin/team/nukiya/demention/global/security/auth/AuthDetailsService.kt
+++ src/main/kotlin/team/nukiya/demention/global/security/auth/AuthDetailsService.kt
@@ -6,17 +6,17 @@ import org.springframework.security.core.userdetails.UserDetailsService
 import org.springframework.stereotype.Component
 import team.nukiya.demention.domain.user.domain.UserMapper
 import team.nukiya.demention.domain.user.exception.UserNotFoundException
-import team.nukiya.demention.domain.user.repository.UserRepository
+import team.nukiya.demention.domain.user.repository.UserEntityRepository
 import java.util.UUID
 
 @Component
 class AuthDetailsService(
-    private val userRepository: UserRepository,
+    private val userEntityRepository: UserEntityRepository,
     private val userMapper: UserMapper,
 ) : UserDetailsService {
     override fun loadUserByUsername(username: String): UserDetails {
         val userId = UUID.fromString(username)
-        val user = userRepository.findByIdOrNull(userId) ?: throw UserNotFoundException
+        val user = userEntityRepository.findByIdOrNull(userId) ?: throw UserNotFoundException
         return AuthDetails(userMapper.toDomain(user)!!)
     }
 }

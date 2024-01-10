bd3a14c6e9e5e1b82aa2bb01b334a77c685d8000
Kangmin <ehfehf0629@gmail.com>
2024-01-10 12:54:30 +0900

    Chore: (#1) AuthCodeEntityRepository에 함수 추가

--- src/main/kotlin/team/nukiya/demention/domain/auth/repisitory/AuthCodeEntityRepository.kt
+++ src/main/kotlin/team/nukiya/demention/domain/auth/repisitory/AuthCodeEntityRepository.kt
@@ -3,4 +3,7 @@ package team.nukiya.demention.domain.auth.repisitory
 import org.springframework.data.repository.CrudRepository
 import team.nukiya.demention.domain.auth.domain.AuthCodeEntity
 
-interface AuthCodeRepository : CrudRepository<AuthCodeEntity, String>
+interface AuthCodeEntityRepository : CrudRepository<AuthCodeEntity, String> {
+
+    fun findByCodeAndPhoneNumber(code: String, phoneNumber: String): AuthCodeEntity?
+}

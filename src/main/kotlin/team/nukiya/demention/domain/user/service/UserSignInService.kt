200c4e280f3e699a5c3052ce2066876aa57b026b
Kangmin <ehfehf0629@gmail.com>
2024-01-11 20:17:52 +0900

    Feat: (#2) UserSignInService 생성

---
+++ src/main/kotlin/team/nukiya/demention/domain/user/service/UserSignInService.kt
@@ -0,0 +1,22 @@
+package team.nukiya.demention.domain.user.service
+
+import org.springframework.stereotype.Service
+import team.nukiya.demention.domain.user.controller.dto.TokenResponse
+import team.nukiya.demention.infrastructure.jwt.JwtProvider
+
+@Service
+class UserSignInService(
+    private val userReader: UserReader,
+    private val jwtProvider: JwtProvider,
+) {
+
+    fun signIn(phoneNumber: String): TokenResponse {
+
+        val savedUser = userReader.getByPhoneNumber(phoneNumber)
+
+        return jwtProvider.generateAllToken(
+            id = savedUser.id.toString(),
+            authority = savedUser.authority,
+        )
+    }
+}

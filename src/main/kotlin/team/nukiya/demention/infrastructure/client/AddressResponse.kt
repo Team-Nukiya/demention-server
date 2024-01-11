bdd820656c0b59aff5cb099976d58fa0cce7cff2
Kangmin <ehfehf0629@gmail.com>
2024-01-11 20:13:04 +0900

    Feat: (#2) 주소 불러오기 기능 추가

---
+++ src/main/kotlin/team/nukiya/demention/infrastructure/client/AddressResponse.kt
@@ -0,0 +1,22 @@
+package team.nukiya.demention.infrastructure.client
+
+import com.fasterxml.jackson.annotation.JsonProperty
+
+data class AddressResponse(
+    val documents: List<AddressElements>,
+)
+
+data class AddressElements(
+    val address: AddressElement,
+)
+
+data class AddressElement(
+    @JsonProperty("address_name")
+    val addressName: String,
+    @JsonProperty("region_1depth_name")
+    val sido: String,
+    @JsonProperty("region_2depth_name")
+    val gungu: String,
+    @JsonProperty("region_3depth_name")
+    val eupMyeonDong: String,
+)

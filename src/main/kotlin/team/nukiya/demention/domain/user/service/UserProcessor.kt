663e4e2bfd97e79cc7ec7e1532c7a1ca0d1c1964
Kangmin <ehfehf0629@gmail.com>
2024-01-11 20:17:16 +0900

    Feat: (#2) UserProcessor 생성

---
+++ src/main/kotlin/team/nukiya/demention/domain/user/service/UserProcessor.kt
@@ -0,0 +1,20 @@
+package team.nukiya.demention.domain.user.service
+
+import org.springframework.stereotype.Component
+import org.springframework.transaction.annotation.Transactional
+import team.nukiya.demention.domain.user.domain.User
+import team.nukiya.demention.domain.user.domain.UserMapper
+import team.nukiya.demention.domain.user.repository.UserEntityRepository
+
+@Transactional
+@Component
+class UserProcessor(
+    private val userEntityRepository: UserEntityRepository,
+    private val userMapper: UserMapper,
+) {
+
+    fun saveUser(user: User): User =
+        userEntityRepository.save(
+            userMapper.toEntity(user)
+        ).let { userMapper.toDomain(it) }
+}

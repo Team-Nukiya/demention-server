1f4348e904e32823cac83289eab1252d1f1fa747
Kangmin <ehfehf0629@gmail.com>
2024-01-11 20:17:38 +0900

    Feat: (#2) UserSignUpService 생성

---
+++ src/main/kotlin/team/nukiya/demention/domain/user/service/UserSignUpService.kt
@@ -0,0 +1,41 @@
+package team.nukiya.demention.domain.user.service
+
+import org.springframework.stereotype.Service
+import team.nukiya.demention.domain.user.controller.dto.TokenResponse
+import team.nukiya.demention.domain.user.domain.Coordinate
+import team.nukiya.demention.domain.user.domain.User
+import team.nukiya.demention.domain.user.exception.UserAlreadyExistsException
+import team.nukiya.demention.infrastructure.jwt.JwtProvider
+
+@Service
+class UserSignUpService(
+    private val userReader: UserReader,
+    private val userProcessor: UserProcessor,
+    private val jwtProvider: JwtProvider,
+) {
+
+    fun signUp(
+        phoneNumber: String,
+        coordinate: Coordinate,
+    ): TokenResponse {
+
+        if (userReader.existsByPhoneNumber(phoneNumber)) {
+            throw UserAlreadyExistsException
+        }
+
+        val address = userReader.getAddressByCoordinate(coordinate)
+
+        val savedUser = userProcessor.saveUser(
+            User(
+                phoneNumber = phoneNumber,
+                nickName = "김은오",
+                address = address,
+            )
+        )
+
+        return jwtProvider.generateAllToken(
+            id = savedUser.id.toString(),
+            authority = savedUser.authority
+        )
+    }
+}

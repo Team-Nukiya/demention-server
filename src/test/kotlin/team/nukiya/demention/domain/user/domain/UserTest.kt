b79583a2f66cdbc4cff027d2287d19becc2fca02
Kangmin <ehfehf0629@gmail.com>
2024-01-13 22:04:45 +0900

    Test: (main) 랜덤 닉네임 생성 로직 테스트 코드 작성

---
+++ src/test/kotlin/team/nukiya/demention/domain/user/domain/UserTest.kt
@@ -0,0 +1,17 @@
+package team.nukiya.demention.domain.user.domain
+
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
+import team.nukiya.demention.domain.user.domain.User.Companion.NICK_NAME_LENGTH
+
+class UserTest {
+
+    @Test
+    fun `길이가 10인 랜덤 닉네임을 생성한다`() {
+        // when
+        val randomNickName = User.generateRandomNickName()
+
+        // then
+        assertThat(randomNickName).isNotBlank().hasSize(NICK_NAME_LENGTH)
+    }
+}

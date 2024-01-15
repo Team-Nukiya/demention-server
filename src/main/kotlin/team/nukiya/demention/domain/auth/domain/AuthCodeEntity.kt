d56972b7c5c44e9613e4231f454c1227a5da404d
Kangmin <ehfehf0629@gmail.com>
2024-01-15 15:17:00 +0900

    Chore: 인증코드 ttl 수정

--- src/main/kotlin/team/nukiya/demention/domain/auth/domain/AuthCodeEntity.kt
+++ src/main/kotlin/team/nukiya/demention/domain/auth/domain/AuthCodeEntity.kt
@@ -2,9 +2,10 @@ package team.nukiya.demention.domain.auth.domain
 
 import org.springframework.data.annotation.Id
 import org.springframework.data.redis.core.RedisHash
+import org.springframework.data.redis.core.TimeToLive
 import org.springframework.data.redis.core.index.Indexed
 
-@RedisHash(timeToLive = 180)
+@RedisHash(timeToLive = 600)
 class AuthCodeEntity(
     @Id
     val code: String,

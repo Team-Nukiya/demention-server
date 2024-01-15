5be3fb80bbac86daadbd2f43eb7d43a5bb7da3fb
Kangmin <ehfehf0629@gmail.com>
2024-01-15 23:40:40 +0900

    Chore: (#17) 인증 코드 제한 관련 예외 추가

--- src/main/kotlin/team/nukiya/demention/domain/auth/exception/AuthExceptions.kt
+++ src/main/kotlin/team/nukiya/demention/domain/auth/exception/AuthExceptions.kt
@@ -5,3 +5,7 @@ import team.nukiya.demention.global.error.CustomException
 object WrongAuthCodeException : CustomException(401, "잘못된 인증 코드 입니다.")
 
 object AuthCodeNotFoundException : CustomException(404, "인증 코드를 찾지 못했습니다.")
+
+object AuthCodeLimitNotFoundException : CustomException(404, "인증 코드 제한 정보를 찾지 못했습니다.")
+
+object AuthCodeOverLimitException : CustomException(429, "인증 코드 제한 횟수보다 더 많이 요청했습니다.")

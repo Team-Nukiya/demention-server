9632638f3b89e79dbab2cb78da5305145ce3d222
Kangmin <ehfehf0629@gmail.com>
2024-01-16 18:47:42 +0900

    Test: (#17) AuthCodeProcessorTest

--- src/test/kotlin/team/nukiya/demention/domain/auth/service/AuthCodeProcessorTest.kt
+++ src/test/kotlin/team/nukiya/demention/domain/auth/service/AuthCodeProcessorTest.kt
@@ -8,10 +8,15 @@ import org.mockito.InjectMocks
 import org.mockito.Mock
 import org.mockito.junit.jupiter.MockitoExtension
 import org.mockito.kotlin.any
+import org.springframework.data.redis.core.RedisTemplate
 import team.nukiya.demention.domain.auth.domain.AuthCode
 import team.nukiya.demention.domain.auth.domain.AuthCodeEntity
+import team.nukiya.demention.domain.auth.domain.AuthCodeLimit
+import team.nukiya.demention.domain.auth.domain.AuthCodeLimitEntity
+import team.nukiya.demention.domain.auth.domain.AuthCodeLimitMapper
 import team.nukiya.demention.domain.auth.domain.AuthCodeMapper
 import team.nukiya.demention.domain.auth.repisitory.AuthCodeEntityRepository
+import team.nukiya.demention.domain.auth.repisitory.AuthCodeLimitEntityRepository
 
 @ExtendWith(MockitoExtension::class)
 class AuthCodeProcessorTest {
@@ -23,6 +28,15 @@ class AuthCodeProcessorTest {
 
     @Mock
     private lateinit var authCodeMapper: AuthCodeMapper
+
+    @Mock
+    private lateinit var authCodeLimitEntityRepository: AuthCodeLimitEntityRepository
+
+    @Mock
+    private lateinit var authCodeLimitMapper: AuthCodeLimitMapper
+
+    @Mock
+    private lateinit var restTemplate: RedisTemplate<String, String>
 
     @Test
     fun `인증 코드 엔티티를 저장하고 인증 코드 도메인으로 변환한다`() {
@@ -40,11 +54,11 @@ class AuthCodeProcessorTest {
             phoneNumber = phoneNumber,
         )
 
-        given(authCodeMapper.toEntity(any()))
-            .willReturn(authCodeEntity)
-
         given(authCodeMapper.toDomain(any()))
             .willReturn(authCode)
+
+        given(authCodeMapper.toEntity(any()))
+            .willReturn(authCodeEntity)
 
         given(authCodeEntityRepository.save(any()))
             .willReturn(authCodeEntity)
@@ -55,4 +69,34 @@ class AuthCodeProcessorTest {
         // then
         assertThat(savedAuthCode).usingRecursiveComparison().isEqualTo(authCode)
     }
+
+    @Test
+    fun `인증 코드 제한 엔티티를 저장하고 인증 코드 제한 도메인으로 변환한다`() {
+        // given
+        val authCodeLimit = AuthCodeLimit(
+            phoneNumber = "010xxxxxxxx",
+            limit = 1,
+        )
+
+        val authCodeLimitEntity = AuthCodeLimitEntity(
+            phoneNumber = "010xxxxxxxx",
+            limit = 1,
+        )
+
+        given(authCodeLimitMapper.toDomain(any()))
+            .willReturn(authCodeLimit)
+
+        given(authCodeLimitMapper.toEntity(any()))
+            .willReturn(authCodeLimitEntity)
+
+        given(authCodeLimitEntityRepository.save(any()))
+            .willReturn(authCodeLimitEntity)
+        // when
+        val savedAuthCodeLimit = authCodeProcessor.saveAuthCodeLimit(authCodeLimit)
+
+        // then
+        assertThat(savedAuthCodeLimit).usingRecursiveComparison().isEqualTo(authCodeLimit)
+    }
+
+    // TODO: incrementLimit 테스트 코드 추가
 }

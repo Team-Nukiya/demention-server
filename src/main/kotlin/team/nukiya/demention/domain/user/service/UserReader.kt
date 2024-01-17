db2c41568dbeca05784dee515f6b0d8df94b342e
Kangmin <ehfehf0629@gmail.com>
2024-01-17 15:59:04 +0900

    Feat: (#19) 유저 정보 가져오기

--- src/main/kotlin/team/nukiya/demention/domain/user/service/UserReader.kt
+++ src/main/kotlin/team/nukiya/demention/domain/user/service/UserReader.kt
@@ -1,8 +1,10 @@
 package team.nukiya.demention.domain.user.service
 
+import org.springframework.security.core.context.SecurityContextHolder
 import org.springframework.stereotype.Component
 import org.springframework.transaction.annotation.Transactional
 import team.nukiya.demention.domain.user.domain.Coordinate
+import team.nukiya.demention.domain.user.domain.User
 import team.nukiya.demention.domain.user.domain.UserMapper
 import team.nukiya.demention.domain.user.repository.UserEntityRepository
 import team.nukiya.demention.infrastructure.client.address.GetAddressService
@@ -20,10 +22,13 @@ class UserReader(
             userMapper.toDomain(it)
         }
 
-
     fun existsByPhoneNumber(phoneNumber: String) =
         userEntityRepository.existsByPhoneNumber(phoneNumber)
 
     fun getAddressByCoordinate(coordinate: Coordinate) =
         getAddressService.getAddressByCoordinate(coordinate)
+
+    fun getCurrentUser(): User {
+        return SecurityContextHolder.getContext().authentication.principal as User
+    }
 }

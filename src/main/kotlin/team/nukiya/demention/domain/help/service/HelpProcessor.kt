76162460ae11c545065928e68542133fb9c51e89
Kangmin <ehfehf0629@gmail.com>
2024-01-17 15:56:24 +0900

    Feat: (#19) HelpProcessor 생성

---
+++ src/main/kotlin/team/nukiya/demention/domain/help/service/HelpProcessor.kt
@@ -0,0 +1,20 @@
+package team.nukiya.demention.domain.help.service
+
+import org.springframework.stereotype.Component
+import org.springframework.transaction.annotation.Transactional
+import team.nukiya.demention.domain.help.domain.Help
+import team.nukiya.demention.domain.help.domain.HelpMapper
+import team.nukiya.demention.domain.help.repository.HelpEntityRepository
+
+@Transactional
+@Component
+class HelpProcessor(
+    private val helpEntityRepository: HelpEntityRepository,
+    private val helpMapper: HelpMapper,
+) {
+
+    fun saveHelp(help: Help) =
+        helpEntityRepository.save(
+            helpMapper.toEntity(help)
+        ).let { helpMapper.toDomain(it) }
+}

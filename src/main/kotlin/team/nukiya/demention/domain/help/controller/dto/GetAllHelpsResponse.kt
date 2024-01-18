9640373decbd2e63690a2cb0b5253ca979b08d03
Kangmin <ehfehf0629@gmail.com>
2024-01-18 20:00:49 +0900

    Feat: (#21) 공고 관련 Response 모델 생성

---
+++ src/main/kotlin/team/nukiya/demention/domain/help/controller/dto/GetAllHelpsResponse.kt
@@ -0,0 +1,7 @@
+package team.nukiya.demention.domain.help.controller.dto
+
+import team.nukiya.demention.domain.help.domain.AllHelp
+
+data class GetAllHelpsResponse(
+    val allHelps: List<AllHelp>,
+)
---
+++ src/main/kotlin/team/nukiya/demention/domain/help/controller/dto/GetHelpDetailsResponse.kt
@@ -0,0 +1,7 @@
+package team.nukiya.demention.domain.help.controller.dto
+
+import team.nukiya.demention.domain.help.domain.HelpDetails
+
+data class GetHelpDetailsResponse(
+    val helpDetails: HelpDetails,
+)

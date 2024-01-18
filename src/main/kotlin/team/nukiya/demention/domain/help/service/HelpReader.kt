c108f8c0b80e3629aedbbdbc55c284e1459fee72
Kangmin <ehfehf0629@gmail.com>
2024-01-18 19:39:59 +0900

    Feat: (#21) 공고 상세 조회

--- src/main/kotlin/team/nukiya/demention/domain/help/service/HelpReader.kt
+++ src/main/kotlin/team/nukiya/demention/domain/help/service/HelpReader.kt
@@ -1,10 +1,15 @@
 package team.nukiya.demention.domain.help.service
 
+import com.querydsl.jpa.impl.JPAQueryFactory
 import org.springframework.data.repository.findByIdOrNull
 import org.springframework.stereotype.Component
 import org.springframework.transaction.annotation.Transactional
+import team.nukiya.demention.domain.help.domain.HelpDetails
 import team.nukiya.demention.domain.help.domain.HelpMapper
+import team.nukiya.demention.domain.help.domain.QQueryHelpDetailsVO
+import team.nukiya.demention.domain.help.domain.QHelpEntity.helpEntity
 import team.nukiya.demention.domain.help.repository.HelpEntityRepository
+import team.nukiya.demention.domain.user.domain.QUserEntity.userEntity
 import java.util.UUID
 
 @Transactional(readOnly = true)
@@ -12,8 +17,30 @@ import java.util.UUID
 class HelpReader(
     private val helpEntityRepository: HelpEntityRepository,
     private val helpMapper: HelpMapper,
+    private val jpaQueryFactory: JPAQueryFactory,
 ) {
     fun getHelpById(helpId: UUID) =
         helpEntityRepository.findByIdOrNull(helpId)
             ?.let { helpMapper.toDomain(it) }
+
+    fun getHelpDetailsById(helpId: UUID): HelpDetails? =
+        jpaQueryFactory
+            .select(
+                QQueryHelpDetailsVO(
+                    helpEntity.id,
+                    helpEntity.title,
+                    helpEntity.content,
+                    helpEntity.compensation,
+                    helpEntity.helpImageUrl,
+                    helpEntity.helpStatus,
+                    helpEntity.helpStartDateTime,
+                    helpEntity.helpEndDateTime,
+                    helpEntity.modifiedDateTime,
+                    userEntity.addressName,
+                    userEntity.nickName,
+                )
+            )
+            .from(helpEntity)
+            .join(helpEntity.userEntity, userEntity)
+            .fetchOne()
 }

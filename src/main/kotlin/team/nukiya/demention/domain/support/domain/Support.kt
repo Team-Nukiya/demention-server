f38b0dacce2813c34102b08183d97d377f6984f2
alsdl0629 <ehfehf0629@gmail.com>
2024-01-22 18:36:33 +0900

    Feat: (#23) 지원 취소 가능한지 확인

--- src/main/kotlin/team/nukiya/demention/domain/support/domain/Support.kt
+++ src/main/kotlin/team/nukiya/demention/domain/support/domain/Support.kt
@@ -1,5 +1,7 @@
 package team.nukiya.demention.domain.support.domain
 
+import team.nukiya.demention.domain.support.domain.SupportStatus.SUPPORTING
+import team.nukiya.demention.domain.support.exception.SupportCanNotCancelException
 import java.time.LocalDateTime
 import java.util.UUID
 
@@ -8,5 +10,11 @@ data class Support(
     val userId: UUID,
     val helpId: UUID,
     val supportStatus: SupportStatus,
-    val createdDateTime: LocalDateTime,
-)
+    val createdDateTime: LocalDateTime = LocalDateTime.now(),
+) {
+    fun checkCancellable() {
+        if (this.supportStatus != SUPPORTING) {
+            throw SupportCanNotCancelException
+        }
+    }
+}

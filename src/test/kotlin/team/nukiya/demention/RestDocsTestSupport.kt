298b0169072d59984272ddc1d7d475ecfb64c3bf
alsdl0629 <ehfehf0629@gmail.com>
2024-01-26 00:15:49 +0900

    Feat: (#25) RestDocsTestSupport

---
+++ src/test/kotlin/team/nukiya/demention/RestDocsTestSupport.kt
@@ -0,0 +1,52 @@
+package team.nukiya.demention
+
+import com.fasterxml.jackson.databind.ObjectMapper
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Disabled
+import org.junit.jupiter.api.extension.ExtendWith
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.context.annotation.Import
+import org.springframework.restdocs.RestDocumentationContextProvider
+import org.springframework.restdocs.RestDocumentationExtension
+import org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.documentationConfiguration
+import org.springframework.restdocs.mockmvc.RestDocumentationResultHandler
+import org.springframework.test.web.servlet.MockMvc
+import org.springframework.test.web.servlet.result.MockMvcResultHandlers
+import org.springframework.test.web.servlet.setup.DefaultMockMvcBuilder
+import org.springframework.test.web.servlet.setup.MockMvcBuilders
+import org.springframework.transaction.annotation.Transactional
+import org.springframework.web.context.WebApplicationContext
+import org.springframework.web.filter.CharacterEncodingFilter
+
+
+@Disabled
+@Transactional
+@AutoConfigureMockMvc
+@Import(RestDocsConfiguration::class)
+@ExtendWith(RestDocumentationExtension::class)
+@SpringBootTest
+abstract class RestDocsTestSupport {
+    @Autowired
+    protected lateinit var restDocs: RestDocumentationResultHandler
+
+    @Autowired
+    protected lateinit var mockMvc: MockMvc
+
+    @Autowired
+    protected lateinit var objectMapper: ObjectMapper
+
+    @BeforeEach
+    fun setUp(
+        context: WebApplicationContext,
+        provider: RestDocumentationContextProvider,
+    ) {
+        this.mockMvc = MockMvcBuilders.webAppContextSetup(context)
+            .apply<DefaultMockMvcBuilder>(documentationConfiguration(provider))
+            .alwaysDo<DefaultMockMvcBuilder>(MockMvcResultHandlers.print())
+            .alwaysDo<DefaultMockMvcBuilder>(restDocs)
+            .addFilters<DefaultMockMvcBuilder>(CharacterEncodingFilter("UTF-8", true))
+            .build()
+    }
+}

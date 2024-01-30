8f12a08fcd3dab523c8f924a6834de4484714874
Kangmin <ehfehf0629@gmail.com>
2024-01-30 11:01:00 +0900

    Chore: (#27) SupportIsNotDone 예외 추가

--- src/main/kotlin/team/nukiya/demention/domain/support/exception/SupportExceptions.kt
+++ src/main/kotlin/team/nukiya/demention/domain/support/exception/SupportExceptions.kt
@@ -4,6 +4,8 @@ import team.nukiya.demention.global.error.CustomException
 
 object SupportCanNotCancelException : CustomException(403, "지원을 취소할 수 없습니다.")
 
+object SupportIsNotDoneException : CustomException(403, "봉사를 완료하지 않았습니다.")
+
 object SupportNotFountException : CustomException(404, "지원 내역을 찾지 못했습니다.")
 
 object AlreadySupportException : CustomException(409, "중복 지원이 불가합니다.")
